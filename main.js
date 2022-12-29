613852114273ae5aaf136baea3d44efe48290b9c
TeaChareqishvili <teachareqishvili89@gmail.com>
2022-12-29 21:40:02 +0400

    css and js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
 
 window.addEventListener('keydown', function(e){
-    console.log(e.keyCode);
-      const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    // // const audio = document.querySelector('audio[data-key="65"]')
-     console.log(audio);
+   
+       const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    //  console.log(audio);
+      if(!audio) return;
+     audio.play();
 })
 

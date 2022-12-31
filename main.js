06e7110b12becc86ea66abd42bc82d200192b423
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-01-01 02:33:10 +0400

    finish

--- main.js
+++ main.js
@@ -3,8 +3,19 @@
 window.addEventListener('keydown', function(e){
    
        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    //  console.log(audio);
+       const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+  
       if(!audio) return;
-     audio.play();
+      audio.currentTime=0;
+     
+      audio.play();
+      key.classList.add('playing');
+   
+   
 })
 
+window.addEventListener('keyup',function(elem){
+    const key = document.querySelector(`div[data-key="${elem.keyCode}"]`);
+    key.classList.remove('playing');
+})
